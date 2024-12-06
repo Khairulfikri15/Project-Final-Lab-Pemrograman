43ad76afe7e83a7307768106fcb71cdfb84344d8
waazcode <fawwaz24@mhs.usk.ac.id>
2024-12-06 21:37:00 +0700

    header.h

---
+++ Sistem Lab/header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+int menuLogin(int *admin);
+int lihatList();
+int pinjamAlat();
+int lihatPinjam();
+int pengembalian();
+int tambahData();
+int hapusData();
+int editData();
+
+#endif
